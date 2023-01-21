52f9b2bc4a8bafe2b8895306e1b97b0c9af2e4f9
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2023-01-22 07:17:45 +0900

    nugetからのPackageReferenceで参照した場合の挙動を見るプロジェクトを追加

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Globalization;
+
+var cult = CultureInfo.GetCultureInfo("ja-JP");
+
+
+CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
+
+Console.WriteLine(DateTime.Now);
